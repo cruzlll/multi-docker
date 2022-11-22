55f9b12793dacceb224e618580601d75457151be
cruzlll <leocruz0899@gmail.com>
2022-11-22 18:48:07 +0800

    my changes

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,8 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Fib Calculator
-          </a>
+          <title> Fib Calculator</title>
+      
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
